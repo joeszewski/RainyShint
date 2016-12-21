3ea5dffa2fe6338dc6e3365661230f722d2e2e46
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-12-20 20:58:12 -0600

    created an arrary to hold the downloaded forecast data, and a function to parse through the array and add each element to the TableView

---
+++ RainyShinyCloudy/WeatherCell.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherCell.swift
+//  RainyShinyCloudy
+//
+//  Created by Joe Janiszewski on 12/20/16.
+//  Copyright © 2016 Joe Janiszewski. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    @IBOutlet weak var highTemp: UILabel!
+    @IBOutlet weak var lowTemp: UILabel!
+    
+    func configureCell(forecast: Forecast) {
+        lowTemp.text = "\(forecast.lowTemp)"
+        highTemp.text = "\(forecast.highTemp)"
+        weatherType.text = forecast.weatherType
+        dayLabel.text = forecast.date
+        weatherIcon.image = UIImage(named: forecast.weatherType)
+        
+    }
+}
